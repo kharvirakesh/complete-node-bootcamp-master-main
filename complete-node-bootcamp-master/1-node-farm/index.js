6d5f500a54baa6668c56ac8c84a32ffdcbf742ff
kharvirakesh <kharvirakesh3@gmail.com>
2023-08-28 19:46:57 +0530

    FEAT : Filling the template

--- complete-node-bootcamp-master/1-node-farm/index.js
+++ complete-node-bootcamp-master/1-node-farm/index.js
@@ -5,26 +5,52 @@ const fs = require('fs');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%ID%}/g, product.id);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+
+    if (!product.organic) output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+};
+
+const tempOverview = fs.readFileSync(`${__dirname}/starter/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/starter/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/starter/template-product.html`, 'utf-8');
+
+const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
     const pathName = req.url;
 
+    // overview page
     if (pathName === '/' || pathName === '/overview') {
-        res.end('home page');
-    } else if (pathName === '/api') {
-        fs.readFile(`${__dirname}/final/dev-data/data.json`, 'utf-8', (err, data) => {
-            console.log(JSON.parse(data));
-            res.writeHead(200, { 'Content-type': 'Application/json' });
-            res.end(data);
-        })
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el));
+        res.end(tempOverview);
     }
+    // api page
+    else if (pathName === '/api') {
+        res.writeHead(200, { 'Content-type': 'application/json' });
+        res.end(data);
+    }
+    // product page
     else if (pathName === '/product') {
         res.end('product page');
-    } else {
+    }
+    // not found!
+    else {
         res.writeHead(404, {
-            'Content-type': 'plain/html',
+            'Content-type': 'text/html',
             'my-own-header': 'hello-world',
-        })
-        res.end(`<h1>page not found!</h1>`);
+        });
+        res.end('<h1>page not found!</h1>');
     }
 });
 
