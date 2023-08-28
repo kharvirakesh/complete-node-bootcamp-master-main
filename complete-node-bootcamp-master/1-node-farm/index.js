69f0e56dddc4b11a2346b09499d29ebb23213888
kharvirakesh <kharvirakesh3@gmail.com>
2023-08-28 16:52:50 +0530

    FEAT : build a simple api

--- complete-node-bootcamp-master/1-node-farm/index.js
+++ complete-node-bootcamp-master/1-node-farm/index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const url = require('url');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -7,18 +8,20 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     const pathName = req.url;
 
-    // res.statusCode = 200;
-    // res.setHeader('Content-Type', 'text/plain');
-    // console.log(req.url);
-    // res.end('pathName');
-
     if (pathName === '/' || pathName === '/overview') {
         res.end('home page');
-    } else if (pathName === '/product') {
+    } else if (pathName === '/api') {
+        fs.readFile(`${__dirname}/final/dev-data/data.json`, 'utf-8', (err, data) => {
+            console.log(JSON.parse(data));
+            res.writeHead(200, { 'Content-type': 'Application/json' });
+            res.end(data);
+        })
+    }
+    else if (pathName === '/product') {
         res.end('product page');
     } else {
         res.writeHead(404, {
-            'Content-type':'plain/html',
+            'Content-type': 'plain/html',
             'my-own-header': 'hello-world',
         })
         res.end(`<h1>page not found!</h1>`);
