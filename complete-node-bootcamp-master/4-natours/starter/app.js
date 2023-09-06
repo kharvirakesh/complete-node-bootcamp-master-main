6c9a4c8d54b5cc5533572619c5866405c7aff390
kharvirakesh <kharvirakesh3@gmail.com>
2023-09-06 16:17:10 +0530

    POST metod added

--- complete-node-bootcamp-master/4-natours/starter/app.js
+++ complete-node-bootcamp-master/4-natours/starter/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
+app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`../starter/dev-data/data/tours-simple.json`),
@@ -17,15 +18,41 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `../starter/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tours: newTour,
+        },
+      });
+    },
+  );
+});
+
+app.get('/api/v1/tour', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
 app.get('/', (req, res) => {
   res.status(200).json({
     message: 'Hello from the server side!',
     app: 'natours',
   });
-});
-
-app.post('/', (req, res) => {
-  res.status(200).end('you can post this endpoint');
 });
 
 const port = 3000;
