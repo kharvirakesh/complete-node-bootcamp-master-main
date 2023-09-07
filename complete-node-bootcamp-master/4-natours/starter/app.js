31f66a5aa2b94c2470b661e050c5f684adecb28b
kharvirakesh <kharvirakesh3@gmail.com>
2023-09-07 13:15:13 +0530

    PATCH implemented

--- complete-node-bootcamp-master/4-natours/starter/app.js
+++ complete-node-bootcamp-master/4-natours/starter/app.js
@@ -38,6 +38,22 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+app.patch('/api/v1/tours:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid id',
+    });
+  }
+
+  res.status(200).json({
+    status: 'status',
+    data: {
+      tours: '<Updated tour here...>',
+    },
+  });
+});
+
 app.get('/api/v1/tour', (req, res) => {
   res.status(200).json({
     status: 'success',
